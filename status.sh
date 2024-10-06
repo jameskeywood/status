eed8904703573e43e481ed774773aae0c734fc04
jameskeywood <jskeywood@gmail.com>
2024-10-06 14:24:58 +0100

    Add brightness and volume

--- status.sh
+++ status.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
 while true  
 do
-	BATTERY=$(cat /sys/class/power_supply/BAT0/capacity)
-	xsetroot -name "$(date) ${BATTERY}%"
+	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+	VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o [0-9].[0-9][0-9])
+	BRI=$(light)
+	xsetroot -name " $(date) BRI:${BRI} VOL:${VOL} BAT:${BAT}% "
 	sleep 1
 done
 
