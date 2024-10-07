ba296e43da21d125224e29ae3c7af619199a0866
jameskeywood <jskeywood@gmail.com>
2024-10-07 23:59:20 +0100

    Add connectivity section and pipeline dividers

--- status.sh
+++ status.sh
@@ -1,9 +1,10 @@
 while true  
 do
-	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
-	VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o [0-9].[0-9][0-9])
+	CON=$(nmcli | grep -o "[^ ]*$" -m 1)
 	BRI=$(light)
-	xsetroot -name " $(date) BRI:${BRI} VOL:${VOL} BAT:${BAT}% "
+	VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o [0-9].[0-9][0-9])
+	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
+	xsetroot -name " $(date) | ${CON} | BRI:${BRI} | VOL:${VOL} | BAT:${BAT}% "
 	sleep 1
 done
 
