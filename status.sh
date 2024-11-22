b4f9c005f547fbb7f45449c764dc9938ed2e354b
jameskeywood <jskeywood@gmail.com>
2024-11-22 21:42:09 +0000

    Add bluetooth section

--- status.sh
+++ status.sh
@@ -1,10 +1,11 @@
 while true  
 do
 	CON=$(nmcli | grep -o "[^ ]*$" -m 1)
+	BLU=$(bluetoothctl devices Connected | cut -d' ' -f3-)
 	BRI=$(light)
 	VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o [0-9].[0-9][0-9])
 	BAT=$(cat /sys/class/power_supply/BAT0/capacity)
-	xsetroot -name " $(date) | ${CON} | BRI:${BRI} | VOL:${VOL} | BAT:${BAT}% "
+	xsetroot -name " $(date) | CON:${CON} | BLU:${BLU[0]} | BRI:${BRI} | VOL:${VOL} | BAT:${BAT}% "
 	sleep 1
 done
 
